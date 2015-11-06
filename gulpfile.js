986f6e2fd7ddd0fee9aeda59d42561d6249afc13
Eric Stout <eric@factor1studios.com>
2015-11-06 11:01:23 -0700

    Fixes typo in sass watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ gulp.task('copy', function() {
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('src/js/*.js', ['lint', 'scripts']);
-    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/scss/*.scss', ['sass']);
     gulp.watch('src/css/*.css', ['minify-css']);
     gulp.watch('src/images/src/*.{jpg,png}', ['images']);
 });
