45d07b4dbec749bbc7688620483c5bdda16b8bec
Alexander Persky <info@alexanderpersky.com>
2016-01-16 21:11:07 -0700

    Updated gulpfile.js
    - updated to match the new directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,116 +1,117 @@
-// Theme Specific Variables
-var theme = 'your-theme-name';
+/*------------------------------------------------------------------------------
+  Gulpfile.js
+------------------------------------------------------------------------------*/
+// Theme specific variables
+var theme        = 'your-theme-name';
+var phpFiles     = [];
+var htmlFiles    = [];
+var cssFiles     = ['src/assets/css/*.css', '!src/assets/css/*.min.css'];
+var sassFiles    = ['src/assets/scss/theme.scss', 'src/assets/scss/**/*'];
+var jsFiles      = ['src/assets/js/*.js', '!src/assets/js/theme.js', '!src/assets/js/theme.min.js'];
+var imageFiles   = ['src/assets/img/*.{jpg,png,gif}'];
+var concatFiles  = ['src/assets/js/theme.js'];
+var copyFiles    = ['!src/assets/img/*', 'src/**/*'];
 
 // Include gulp
-var gulp = require('gulp');
+var gulp         = require('gulp');
 
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var nano = require('gulp-cssnano');
-// var sourcemaps = require('gulp-sourcemaps');
+// Include plugins
+var jshint       = require('gulp-jshint');
+var sass         = require('gulp-sass');
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify');
+var rename       = require('gulp-rename');
+var imagemin     = require('gulp-imagemin');
+var pngquant     = require('imagemin-pngquant');
+var nano         = require('gulp-cssnano');
+var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-var zip = require('gulp-zip');
+var zip          = require('gulp-zip');
 
+/*------------------------------------------------------------------------------
+  Development Tasks
+------------------------------------------------------------------------------*/
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src([
-    	'src/js/*.js',
-    	'!src/js/global.js',
-    	'!src/js/global.min.js'
-    	])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src( jsFiles )
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('src/scss/theme.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('src/css/'));
+  return gulp.src( sassFiles )
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest( 'src/assets/css' ));
 });
 
+/*------------------------------------------------------------------------------
+  Production Tasks
+------------------------------------------------------------------------------*/
 // Minimize CSS
 gulp.task('minify-css', ['sass'], function() {
-  	return gulp.src([
-  		'src/css/*.css',
-  		'!src/css/*.min.css'
-  		])
-	  	.pipe(rename({
-		        suffix: '.min'
-	        }))
-	    .pipe(nano({
-        discardComments: {removeAll: true},
-        autoprefixer: false
-      }))
-	    .pipe(gulp.dest('src/css/'));
+	return gulp.src( cssFiles )
+  	.pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(nano({
+      discardComments: {removeAll: true},
+      autoprefixer: false
+    }))
+    .pipe(gulp.dest( 'src/assets/css' ));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', ['lint'], function() {
-    return gulp.src([
-    	'src/js/*.js',
-    	'!src/js/global.js',
-    	'!src/js/global.min.js'
-    	])
-        .pipe(concat('global.js'))
-        .pipe(gulp.dest('src/js'))
-        .pipe(rename('global.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('src/js'));
+  return gulp.src( jsFiles )
+    .pipe(concat( concatFiles ))
+    .pipe(gulp.dest('src/assets/js'))
+    .pipe(rename('theme.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest( 'src/assets/js' ));
 });
 
 // Minimize Images
 gulp.task('images', function() {
-    return gulp.src('src/images/src/*.{jpg,png,gif}')
-    .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-    .pipe(gulp.dest('src/images'));
+  return gulp.src( imageFiles )
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest( 'src/assets/img' ));
 });
 
 // Copy Essential Files To Dist
 gulp.task('copy', function() {
-	gulp.src([
-		// set up what you want to copy or ignore
-		'!src/images/src/*',
-		'src/**/*'
-	])
-	.pipe(gulp.dest(theme));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch([
-    	'src/js/*.js',
-    	'!src/js/global.js',
-    	'!src/js/global.min.js'
-    	], ['scripts']);
-    gulp.watch('src/scss/**/*.scss', ['styles']);
-    gulp.watch('src/css/*.css', ['styles']);
-    gulp.watch('src/images/src/*.{jpg,png}', ['images']);
+	gulp.src( copyFiles )
+	.pipe(gulp.dest( theme ));
 });
 
 // Package a zip for theme upload
 gulp.task('package', function() {
-	return gulp.src(theme+'/**/*')
-		.pipe(zip(theme+'.zip'))
-		.pipe(gulp.dest('./'));
+	return gulp.src( theme + '/**/*' )
+		.pipe(zip( theme + '.zip' ))
+		.pipe(gulp.dest( './' ));
 });
 
 // Styles Task - minify-css is the only task we call, because it is dependent upon sass running first.
 gulp.task('styles', ['minify-css']);
 
+/*------------------------------------------------------------------------------
+  Default Tasks
+------------------------------------------------------------------------------*/
 // Default Task
 gulp.task('default', ['styles', 'scripts', 'images', 'copy', 'watch']);
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch([ jsFiles ], ['scripts']);
+  gulp.watch( sassFiles, ['styles']);
+  gulp.watch( cssFiles, ['styles']);
+  gulp.watch( imageFiles, ['images']);
+});
