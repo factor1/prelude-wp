f1572a3e9dd420ebf91a548a82717a86fda9a89d
Alexander Persky <info@alexanderpersky.com>
2016-01-22 20:29:23 -0700

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,78 @@
 /*------------------------------------------------------------------------------
   Gulpfile.js
 ------------------------------------------------------------------------------*/
-// Theme specific variables
+// Name your theme
 var theme        = 'your-theme-name';
-var phpFiles     = [];
-var htmlFiles    = [];
-var cssFiles     = ['src/assets/css/*.css', '!src/assets/css/*.min.css'];
-var sassFiles    = ['src/assets/scss/theme.scss', 'src/assets/scss/**/*'];
-var jsFiles      = ['src/assets/js/*.js', '!src/assets/js/theme.js', '!src/assets/js/theme.min.js'];
-var imageFiles   = ['src/assets/img/*.{jpg,png,gif}'];
-var concatFiles  = ['src/assets/js/theme.js'];
-var copyFiles    = ['!src/assets/img/*', 'src/**/*'];
+
+// Set the paths you will be working with
+var phpFiles     = [],
+    htmlFiles    = [],
+    cssFiles     = ['src/assets/css/*.css', '!src/assets/css/*.min.css'],
+    sassFiles    = ['src/assets/scss/**/*.scss'],
+    jsFiles      = ['src/assets/js/theme.js'],
+    imageFiles   = ['src/assets/img/*.{jpg,png,gif}'],
+    concatFiles  = [],
+    copyFiles    = ['!src/assets/img/**/*', 'src/**/*'],
+    url          = 'wp-dev:8888'; // See https://browsersync.io/docs/options/#option-proxy
 
 // Include gulp
 var gulp         = require('gulp');
 
 // Include plugins
-var jshint       = require('gulp-jshint');
-var sass         = require('gulp-sass');
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify');
-var rename       = require('gulp-rename');
-var imagemin     = require('gulp-imagemin');
-var pngquant     = require('imagemin-pngquant');
-var nano         = require('gulp-cssnano');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var zip          = require('gulp-zip');
+var jshint       = require('gulp-jshint'),
+    sass         = require('gulp-sass'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    rename       = require('gulp-rename'),
+    imagemin     = require('gulp-imagemin'),
+    pngquant     = require('imagemin-pngquant'),
+    nano         = require('gulp-cssnano'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync  = require('browser-sync'),
+    plumber      = require('gulp-plumber'),
+    stylish      = require('jshint-stylish');
+    zip          = require('gulp-zip');
 
 /*------------------------------------------------------------------------------
   Development Tasks
 ------------------------------------------------------------------------------*/
-// Lint Task
-gulp.task('lint', function() {
-  return gulp.src( jsFiles )
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+// Launch a development server
+gulp.task( 'serve', function() {
+  browserSync.init({
+    proxy: url
+      // port: 3000
+  });
 });
 
-// Compile Our Sass
+// Compile Sass
 gulp.task('sass', function() {
   return gulp.src( sassFiles )
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest( 'src/assets/css' ));
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest( 'src/assets/css' ))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// Lint JavaScript
+gulp.task('lint', function() {
+  return gulp.src( jsFiles )
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
+    .pipe(sourcemaps.write())
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 /*------------------------------------------------------------------------------
@@ -62,33 +88,38 @@ gulp.task('minify-css', ['sass'], function() {
       discardComments: {removeAll: true},
       autoprefixer: false
     }))
-    .pipe(gulp.dest( 'src/assets/css' ));
+    .pipe(gulp.dest( 'src/assets/css' ))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-// Concatenate & Minify JS
+// Concatenate & Minify JavaScript
 gulp.task('scripts', ['lint'], function() {
-  return gulp.src( jsFiles )
-    .pipe(concat( concatFiles ))
+  return gulp.src( concatFiles )
+    .pipe(concat( 'all.js' ))
     .pipe(gulp.dest('src/assets/js'))
     .pipe(rename('theme.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest( 'src/assets/js' ));
 });
 
-// Minimize Images
+// Compress Images
 gulp.task('images', function() {
   return gulp.src( imageFiles )
+  .pipe(plumber())
   .pipe(imagemin({
     progressive: true,
+    interlaced: true,
     svgoPlugins: [{removeViewBox: false}],
     use: [pngquant()]
   }))
   .pipe(gulp.dest( 'src/assets/img' ));
 });
 
-// Copy Essential Files To Dist
+// Copy essential files to Dist
 gulp.task('copy', function() {
-	gulp.src( copyFiles )
+	return gulp.src( copyFiles )
 	.pipe(gulp.dest( theme ));
 });
 
@@ -106,12 +137,17 @@ gulp.task('styles', ['minify-css']);
   Default Tasks
 ------------------------------------------------------------------------------*/
 // Default Task
-gulp.task('default', ['styles', 'scripts', 'images', 'copy', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'images', 'serve', 'watch']);
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-  gulp.watch([ jsFiles ], ['scripts']);
-  gulp.watch( sassFiles, ['styles']);
+  gulp.watch( sassFiles, ['sass']);
+  gulp.watch( jsFiles, ['scripts']);
   gulp.watch( cssFiles, ['styles']);
-  gulp.watch( imageFiles, ['images']);
+  gulp.watch( imageFiles, ['images'], browserSync.reload );
+  gulp.watch( phpFiles, browserSync.reload );
+  gulp.watch( htmlFiles, browserSync.reload );
 });
+
+// When you are ready to ship
+gulp.task('build', ['minify-css', 'scripts', 'images', 'copy', 'package']);
