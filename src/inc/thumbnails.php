c58cf5a6e1e26b6ce16188c8330330bcd9292e82
Eric Stout <ericwstout@gmail.com>
2016-03-09 11:47:02 -0700

    Adds thumbnails page for custom thumbnail sizes

---
+++ src/inc/thumbnails.php
@@ -0,0 +1,10 @@
+<?php
+/*-----------------------------------------------------------------------------
+  Adds thumbnail support and additional thumbnail sizes
+-----------------------------------------------------------------------------*/
+
+if( function_exists('add_theme_support') ){
+  add_theme_support('post-thumbnails');
+
+  // Use add_image_size below to add additional thumbnail sizes
+}
