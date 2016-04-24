c912d9898eff2eceb9624dc03bb95155455f999c
Eric Stout <ericwstout@gmail.com>
2016-04-23 17:07:12 -0700

    Adds featured image as url function

--- src/inc/thumbnails.php
+++ src/inc/thumbnails.php
@@ -1,4 +1,14 @@
 <?php
+
+/*-----------------------------------------------------------------------------
+  Get featured image as url
+-----------------------------------------------------------------------------*/
+function featuredURL($size){
+  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $size );
+  $url = $thumb['0'];
+  echo $url;
+}
+
 /*-----------------------------------------------------------------------------
   Adds thumbnail support and additional thumbnail sizes
 -----------------------------------------------------------------------------*/
