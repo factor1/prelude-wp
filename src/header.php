76f046e89f8983c759d60eb9a0539f1858ea3ffd
Eric Stout <ericwstout@gmail.com>
2016-07-29 09:40:44 -0700

    Cleans up header file comments, adds theme color meta other 💄 things

--- src/header.php
+++ src/header.php
@@ -1,33 +1,45 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="<?php the_title(); ?>" />
+  <meta property="og:site_name" content="<?php bloginfo('name') ?>">
+
+  <?php
+  /* Theme color for browsers that support it
+  <meta name="theme-color" content="#000">
+  */
+  ?>
+
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php endif; ?>
+
   <?php if (is_search()) { ?>
    <meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
-
-  <meta property="og:title" content="<?php the_title(); ?>" />
-  <meta property="og:site_name" content="<?php bloginfo('name') ?>">
 
   <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
-  <header><!-- Header -->
-    <h1 class="logo">
+  <?php // Header ?>
+  <header>
+
+    <h1>
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
     </h1>
+
     <nav class="main-nav">
       <?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
     </nav>
-    <?php get_search_form(); ?>
+
   </header>
 
   <?php // Main Content ?>
