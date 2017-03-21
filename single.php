7ceada5bffa0ae4b12dbedf7bfdb15ec03ec21de
Eric Stout <ericwstout@gmail.com>
2017-03-21 15:53:18 -0700

    Makes single.php less opinionated

--- single.php
+++ single.php
@@ -1,34 +1,17 @@
 <?php
   /**
    * The single post template.
-   * 
+   *
    * Used when a single post is queried.
    */
   get_header();
-?>
 
-  <section class="main-content">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h1><?php the_title(); ?></h1>
-        <?php
-          get_template_part( 'parts/meta' );
-          if ( has_post_thumbnail() ) {
-            the_post_thumbnail();
-          }
-          the_content();
-          wp_link_pages(
-            array(
-              'before' => 'Pages: ', 'next_or_number' => 'number'
-            ) );
-          the_tags( 'Tags: ', ', ', '' );
-          edit_post_link( 'Edit this entry', '', '.' );
-        ?>
-      </article>
-      <?php
-      comments_template();
-    endwhile; endif; ?>
-  </section>
+  if( have_posts() ):
 
-<?php
+    while( have_posts() ): the_post();
+
+    endwhile;
+    
+  endif;
+
   get_footer();
