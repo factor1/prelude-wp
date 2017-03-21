182b19f97e31cc61b4c3de102e9fdca66578ba47
Eric Stout <ericwstout@gmail.com>
2017-03-21 15:40:00 -0700

    Cleans up functions file

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -1,7 +1,37 @@
 <?php
   /*-----------------------------------------------------------------------------
-    Custom Theme Tweaks
+    Custom Theme Tweaks and Features
   -----------------------------------------------------------------------------*/
+  if ( !function_exists( 'prelude_features' ) ) {
+
+    // Register Theme Features
+    function prelude_features() {
+      // Add theme support for Automatic Feed Links
+      add_theme_support( 'automatic-feed-links' );
+
+      // Add theme support for Post Formats
+      add_theme_support('post-formats', array('status', 'quote', 'gallery', 'image', 'video', 'audio', 'link', 'aside') );
+
+      // Add theme support for Featured Images
+      add_theme_support( 'post-thumbnails' );
+
+      // Add theme support for HTML5 Semantic Markup
+      add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption') );
+
+      // Add theme support for document Title tag
+      add_theme_support( 'title-tag' );
+
+      // Clean up the default WordPress head section
+      remove_action( 'wp_head', 'rsd_link' );
+      remove_action( 'wp_head', 'wlwmanifest_link' );
+      remove_action( 'wp_head', 'wp_generator' );
+      remove_action( 'wp_head', 'start_post_rel_link' );
+      remove_action( 'wp_head', 'index_rel_link' );
+      remove_action( 'wp_head', 'adjacent_posts_rel_link' );
+    }
+    add_action( 'after_setup_theme', 'prelude_features' );
+  }
+  
   // Set the maximum content width for the theme
   function prelude_content_width() {
     $GLOBALS[ 'content_width' ] = apply_filters( 'prelude_content_width', 1200 );
