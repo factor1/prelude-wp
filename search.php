b3ced266aa47ddf30c663a37c8f4643a17ce45c8
Eric Stout <ericwstout@gmail.com>
2017-03-21 15:51:18 -0700

    Makes search results page less opinionated

--- search.php
+++ search.php
@@ -5,29 +5,15 @@
    * Used when a search is performed.
    */
   get_header();
-?>
 
-  <section class="main-content">
-    <?php if ( have_posts() ) : ?>
-      <h2>Search Results</h2>
+  if( have_posts() ):
 
-      <?php the_posts_pagination( array('mid_size' => 2) ); ?>
+    while( have_posts() ): the_post();
 
-      <?php while ( have_posts() ) : the_post(); ?>
-        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <h2><?php the_title(); ?></h2>
-          <?php
-            get_template_part( 'parts/meta' );
-            the_excerpt();
-          ?>
-        </div>
-        <?php
-      endwhile;
-      the_posts_pagination( array('mid_size' => 2) );
-    else : ?>
-      <h2>Sorry, no posts have been found.</h2>
-    <?php endif; ?>
-  </section>
+    endwhile;
 
-<?php
+    the_posts_pagination( array('mid_size' => 2) );
+
+  endif;
+
   get_footer();
