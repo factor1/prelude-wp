41a365fa42d19f006bc063d786a97fb4ad77ec11
Eric Stout <ericwstout@gmail.com>
2017-03-21 15:41:54 -0700

    Removes opinionated header code

--- header.php
+++ header.php
@@ -32,14 +32,6 @@
   <?php // Header ?>
   <header>
 
-    <h1>
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-    </h1>
-
-    <nav class="main-nav">
-      <?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
-    </nav>
-
   </header>
 
   <?php // Main Content ?>
