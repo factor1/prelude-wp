a123adcaed4f7a3d86c0caa8db451a619a97392c
Eric Stout <ericwstout@gmail.com>
2017-03-21 15:49:05 -0700

    Makes page.php less opinionated

--- page.php
+++ page.php
@@ -7,20 +7,11 @@
   get_header();
 ?>
 
-  <section class="main-content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <article id="post-<?php the_ID(); ?>" class="post">
-        <?php if ( has_post_thumbnail() ) {
-          the_post_thumbnail();
-        } ?>
-        <h1><?php the_title(); ?></h1>
-        <?php
-          the_content();
-          edit_post_link( 'Edit this entry.', '<hr><p>', '</p>' );
-        ?>
-      </article>
-    <?php endwhile; endif; ?>
-  </section>
 
-<?php
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+
+    <?php endwhile; endif;
+
   get_footer();
