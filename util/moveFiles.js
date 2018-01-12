c8e7e8fbe1438257a499910fcb7a280790a12fb5
Eric Stout <ericwstout@gmail.com>
2018-01-12 10:45:10 -0700

    Fix exclude file path

--- util/moveFiles.js
+++ util/moveFiles.js
@@ -27,7 +27,7 @@ if( process.env.CI !== 'true' && process.env.TRAVIS !== 'true') {
       if( userInputMove === 'Y' || userInputMove === 'y' || userInputMove === 'yes' ) {
         console.log('You answered yes... Attempting move.'.green);
 
-        exec(`rsync -a -v --ignore-existing --exclude-from 'exclude.txt' ./ ./../../`,(err, stdout, stderr) => {
+        exec(`rsync -a -v --ignore-existing --exclude-from './util/exclude.txt' ./ ./../../`,(err, stdout, stderr) => {
           if(err) {
             console.error(err);
             console.error('There was an error moving the files. Please try manually.'.red);
