8a4562ef30b40d379bbcaf2d23e274be66e52c9f
Eric Stout <ericwstout@gmail.com>
2019-07-17 23:07:20 -0500

    Add more gulp adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const autoprefixer = require("gulp-autoprefixer");
 const babel = require("gulp-babel");
@@ -8,12 +8,13 @@ const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const colors = require("colors");
 const del = require("del");
-const eslint = require('gulp-eslint');
+const eslint = require("gulp-eslint");
 const mode = require("gulp-mode")();
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 const { series, src, dest, watch } = require("gulp");
 
 sass.compiler = require("node-sass");
@@ -22,15 +23,13 @@ const NODE_ENV = process.env.NODE_ENV;
 const URL = process.env.WP_URL;
 
 // Project Paths 
-const phpFiles = ['./**/*.php', './*.php'];
-const cssFiles = './assets/css/';
-const scssFiles = ['./assets/scss/**/*.scss'];
-const jsEntry = './assets/js/src/theme.js';
-const jsOutput = './assets/js/dist/';
-
-// Theme Information 
-const theme = "your-theme-name";
-const version = "0.0.1"; // updated automatically - do not manually update
+const phpFiles = [ "./**/*.php", "./*.php" ];
+const cssFiles = "./assets/css/";
+const scssFiles = [ "./assets/scss/**/*.scss" ];
+const jsEntry = "./assets/js/src/theme.js";
+const jsOutput = "./assets/js/dist/";
+const themeCssFile = "./assets/css/theme.css";
+const imageFiles = [ "./assets/img/*.{jpg,png,gif}" ];
 
 // Compile Sass
 const sassTask = () => {
@@ -80,11 +79,18 @@ const lintJs = () => {
 // clean js dist folder
 const cleanJs = () => del(`${jsOutput}/**/*`);
 
+// compress images 
+const compressImages = () => {
+  return src(imageFiles)
+    .pipe(imagemin())
+    .pipe(dest("./assets/img/"))
+}
+
 // browser sync server
 const server = () => {
   console.log(colors.green.bold(`🛠  Running in ${NODE_ENV} mode`)); // eslint-disable-line no-console
   browserSync.init({
-    proxy: URL ? URL : 'http://localhost:3000'
+    proxy: URL ? URL : "http://localhost:3000"
   });
 
   watch(scssFiles, series(cleanStyles, sassTask, minifyCssTask));
@@ -94,7 +100,7 @@ const server = () => {
 
 module.exports = {
   "minify-css": minifyCssTask,
-  build: series(cleanStyles, cleanJs, sassTask, minifyCssTask, lintJs, compile),
+  build: series(cleanStyles, cleanJs, compressImages, sassTask, minifyCssTask, lintJs, compile),
   clean: series(cleanStyles, cleanJs),
   compile: series(cleanJs, lintJs, compile),
   serve: series(cleanStyles, cleanJs, sassTask, minifyCssTask, lintJs, compile, server),
