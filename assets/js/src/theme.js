40b1d34c47d47a954440f61bca9d0e9246b60199
Eric Stout <ericwstout@gmail.com>
2019-08-22 21:07:16 -0500

    Remove font awesome line from theme js (#165)

--- assets/js/src/theme.js
+++ assets/js/src/theme.js
@@ -1,15 +1,13 @@
 const Bowser = require("bowser");
 
-window.FontAwesomeConfig = { searchPseudoElements: true };
-
-jQuery( document ).ready(function( $ ) {
+jQuery(document).ready(function($) {
   // Inside of this function, $() will work as an alias for jQuery()
   // and other libraries also using $ will not be accessible under this shortcut
   // https://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
 
   // Touch Device Detection
   var isTouchDevice = "ontouchstart" in document.documentElement;
-  if( isTouchDevice ) {
+  if (isTouchDevice) {
     $("body").removeClass("no-touch");
   }
 
@@ -17,10 +15,9 @@ jQuery( document ).ready(function( $ ) {
   const userBrowser = Bowser.getParser(window.navigator.userAgent);
   const browser = userBrowser.getBrowser();
 
-  if( browser.name === "Internet Explorer" && browser.version == "11.0" ) {
+  if (browser.name === "Internet Explorer" && browser.version == "11.0") {
     $("body").addClass("ie-11");
-  } else if ( bowser.safari ) {
+  } else if (bowser.safari) {
     $("body").addClass("safari");
   }
-
 });
